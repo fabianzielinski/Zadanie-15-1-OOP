199fd61e16f4506b7f2dc6284596738ff43729fb
fabianzielinski <fabz@o2.pl>
2019-06-16 00:34:40 +0200

    Add request Self.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-function Phone(brand, price, color) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
+function Button(text) {
+	this.text = text || 'Hello';
 }
 
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function() {
+			alert(self.text);
+		});
+		document.body.appendChild(this.element);
+	}
 }
 
-var SamsungGalaxyS6 = new Phone("Samsung", 2000, "black");
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var OnePlusOne = new Phone("One", 1800, "white");
+var btn1 = new Button('Hello!');
 
-SamsungGalaxyS6.printInfo();
-iPhone6S.printInfo();
-OnePlusOne.printInfo();
+btn1.create();
